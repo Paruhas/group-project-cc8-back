20053b2f05db2aa6e39f6e1bf5d7a2d8a8c1bc26
SupawiriyaSaranarak <xaviel2@gmail.com>
2021-05-01 15:56:38 +0700

    updateMeController

--- controllers/userController.js
+++ controllers/userController.js
@@ -126,7 +126,7 @@ exports.login = async (req, res, next) => {
   }
 };
 
-exports.protect = async (req, res, next) => {
+exports.protectAdmin = async (req, res, next) => {
   try {
     let token = null;
     if (
@@ -145,6 +145,38 @@ exports.protect = async (req, res, next) => {
       },
     });
     if (!user) return res.status(401).json({ message: "user not found" });
+
+    if (user.userRole !== "ADMIN")
+      return res.status(400).json({ message: "You are unauthorized" });
+
+    req.user = user;
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+exports.protectUser = async (req, res, next) => {
+  try {
+    let token = null;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+    if (!token)
+      return res.status(401).json({ message: "you are unauthorized" });
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const user = await User.findOne({
+      where: {
+        id: payload.id,
+      },
+    });
+    if (!user) return res.status(401).json({ message: "user not found" });
+
+    if (user.userRole !== "USER")
+      return res.status(400).json({ message: "You are unauthorized" });
 
     req.user = user;
     next();
