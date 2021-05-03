0f568638b664336830a304097e5ce7fa24dcfc92
apbank2238 <bayulgang@hotmail.com>
2021-05-03 17:51:42 +0700

    add Topic Fn: get without condition

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -1,5 +1,61 @@
 const { Topic } = require("../models");
 
+exports.getAllTopics = async (req, res, next) => {
+  try {
+    const topics = await Topic.findAll({
+      where: {},
+    });
+
+    if (!topics) {
+      return res.status(400).json({ message: "topics not found" });
+    }
+
+    res.status(200).json({ topics });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getTopicById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const topic = await Topic.findOne({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!topic) {
+      return res.status(400).json({ message: "topicById not found" });
+    }
+
+    res.status(200).json({ topic });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getLastedTopics = async (req, res, next) => {
+  try {
+    const topicsLasted = await Topic.findAll({
+      where: {},
+      limit: 4,
+      order: [["created_at", "DESC"]],
+    });
+
+    if (!topicsLasted) {
+      return res.status(400).json({
+        message: "topicsLasted not found",
+      });
+    }
+
+    res.status(200).json({ topicsLasted });
+  } catch (err) {
+    next(err);
+  }
+};
+
 exports.getAllTopicsActive = async (req, res, next) => {
   try {
     const topics = await Topic.findAll({
