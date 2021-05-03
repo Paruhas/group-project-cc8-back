be6f790ecebe319dbe9e88a5f01a80597f27c04b
apbank2238 <bayulgang@hotmail.com>
2021-05-03 17:21:18 +0700

    add Room getFn for status:active

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -24,3 +24,46 @@ exports.getRoomById = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.getAllRoomsActive = async (req, res, next) => {
+  try {
+    const rooms = await Room.findAll({
+      where: {
+        roomStatus: "ACTIVE",
+      },
+    });
+
+    if (!rooms) {
+      return res
+        .status(400)
+        .json({ message: "rooms not found ; or not have active status" });
+    }
+
+    res.status(200).json({ rooms });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getRoomByIdActive = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const room = await Room.findOne({
+      where: {
+        id: id,
+        roomStatus: "ACTIVE",
+      },
+    });
+
+    if (!room) {
+      return res
+        .status(400)
+        .json({ message: "roomById not found ; or not have active status" });
+    }
+
+    res.status(200).json({ room });
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ controllers/topicController.js
@@ -0,0 +1,22 @@
+const { Topic } = require("../models");
+
+exports.getAllTopics = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getTopicById = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getLastedTopic = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
