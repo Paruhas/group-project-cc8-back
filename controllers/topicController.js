a8d13ad8643eeb06041524c19d6cc064d02d61b2
apbank2238 <bayulgang@hotmail.com>
2021-05-04 16:48:24 +0700

    add Fn Topic updateTopicStatus and edit adminRoute

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -75,26 +75,6 @@ exports.getAllTopicsActive = async (req, res, next) => {
     next(err);
   }
 };
-exports.updateTopic = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { topicName, topicContent, topicImg, roomId } = req.body;
-
-    const topic = await Topic.findOne({ where: { id } });
-    if (!topic) return res.status(400).json({ message: "Topic not found." });
-    if (+topic.userId !== +req.user.id)
-      ({ message: "Cannot edit other's topic." });
-    const room = await Room.findOne({ where: { id: roomId } });
-    if (!room) return res.status(400).json({ message: "Room not found." });
-    await Topic.update(
-      { topicName, topicContent, topicImg, roomId },
-      { where: { id } }
-    );
-    res.status(201).json({ message: `Topic id ${id} is updated successfully` });
-  } catch (err) {
-    next(err);
-  }
-};
 
 exports.getTopicByIdActive = async (req, res, next) => {
   try {
@@ -170,6 +150,124 @@ exports.getHotTopicsActive = async (req, res, next) => {
 
     res.status(200).json({ topicsHot });
   } catch (err) {
+    next(err);
+  }
+};
+
+exports.getAllTopicsInactive = async (req, res, next) => {
+  try {
+    const topics = await Topic.findAll({
+      where: {
+        topicStatus: "INACTIVE",
+      },
+    });
+
+    if (!topics) {
+      return res.status(400).json({
+        message: "topicsInactive not found ; or not have inactive status",
+      });
+    }
+
+    res.status(200).json({ topics });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getTopicByIdInactive = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const topic = await Topic.findOne({
+      where: {
+        id: id,
+        topicStatus: "INACTIVE",
+      },
+    });
+
+    if (!topic) {
+      return res.status(400).json({
+        message: "topicByIdInactive not found ; or not have inactive status",
+      });
+    }
+
+    res.status(200).json({ topic });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateTopic = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { topicName, topicContent, topicImg, roomId } = req.body;
+
+    const topic = await Topic.findOne({ where: { id } });
+    if (!topic) return res.status(400).json({ message: "Topic not found." });
+    if (+topic.userId !== +req.user.id)
+      ({ message: "Cannot edit other's topic." });
+    const room = await Room.findOne({ where: { id: roomId } });
+    if (!room) return res.status(400).json({ message: "Room not found." });
+    await Topic.update(
+      { topicName, topicContent, topicImg, roomId },
+      { where: { id } }
+    );
+    res.status(201).json({ message: `Topic id ${id} is updated successfully` });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateTopicStatus = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { topicStatus } = req.body;
+
+    const topic = await Topic.findOne({
+      where: { id },
+    });
+
+    if (!topic) {
+      return res
+        .status(400)
+        .json({ message: "Topic > updateTopicStatus not found." });
+    }
+
+    const IsTopicHasThisStatus = await Topic.findOne({
+      where: {
+        id,
+        topicStatus: topicStatus,
+      },
+    });
+
+    if (IsTopicHasThisStatus) {
+      return res.status(400).json({
+        message:
+          "Topic > can't update this topicStatus ; current topicStatus is same with req.body",
+      });
+    }
+
+    await Topic.update(
+      {
+        topicStatus: topicStatus,
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+
+    res
+      .status(200)
+      .json({ message: `Topic id ${id}'s topicStatus updated successfully` });
+  } catch (err) {
+    if (err.name) {
+      return res.status(500).json({
+        message:
+          "SequelizeDatabaseError: topicStatus's value must be 'ACTIVE' or 'INACTIVE'",
+      });
+    }
     next(err);
   }
 };
