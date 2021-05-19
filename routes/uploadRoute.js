885fed8cac507000e1cb2630d8ce2bdad233393f
apbank2238 <bayulgang@hotmail.com>
2021-05-19 15:58:10 +0700

    delete some not use Fn form upload

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -5,6 +5,5 @@ const multer = require("../middlewares/multer");
 const uploadController = require("../controllers/uploadController");
 
 router.post("/user-img", multer.send, uploadController.uploadUserImage);
-router.post("/room-img", multer.roomIconSend, uploadController.uploadRoomIcon);
 
 module.exports = router;
